5c5753ebbefd63381404541a0f2b18d2c1126a97
Artemis-Hunt <stevetheguy@naver.com>
2020-08-26 23:33:45 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,34 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static void printSeparator() {
+        String separator = "------------------------------------------";
+        System.out.println(separator);
+    }
     public static void main(String[] args) {
         String greet = " Hello! I'm Duke";
         String prompt = " What can I do for you?";
         String goodbye = " Bye. Hope to see you again soon!";
-        String separator = "------------------------------------------";
 
-        System.out.println(separator);
+
+        String input;
+        Scanner in = new Scanner(System.in);
+
+        printSeparator();
         System.out.println(greet);
         System.out.println(prompt);
-        System.out.println(separator);
+        printSeparator();
+
+        input = in.nextLine();
+        while (!(input.equals("bye"))){
+            printSeparator();
+            System.out.println(input);
+            printSeparator();
+            input = in.nextLine();
+        }
+
+        printSeparator();
         System.out.println(goodbye);
-        System.out.println(separator);
+        printSeparator();
     }
 }
